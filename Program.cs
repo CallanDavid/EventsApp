3385008362090cc79308626d37442761bae0f620
Callan Merifield <callan.merifield@gmail.com>
2025-04-07 13:30:19 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace EventsApp
+{
+    public delegate void Notify(string message);
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
